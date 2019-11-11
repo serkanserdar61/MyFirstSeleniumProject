cd27a3b81a2ed09c39588db58cfccecb5182f9bd
serkan serdar <serkanserdar61@gmail.com>
2019-11-11 12:11:24 -0500

    dynamicdropdownbutton

--- src/test/java/tests/day9/dynamicdropsdownorup.java
+++ src/test/java/tests/day9/dynamicdropsdownorup.java
@@ -1,26 +1,25 @@
-package tests.day8;
+package tests.day9;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
-public class dropdown {
+public class dynamicdropsdownorup {
 
     public static void main(String[] args) throws InterruptedException {
-
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.get("https://www.spicejet.com/");
         driver.manage().window().maximize();
         Thread.sleep(3000);
-        Select option=new Select(driver.findElement(By.id("flightSearchForm.adultOrSeniorPassengerCount")));
-        option.selectByValue("2");
-        option.selectByIndex(5);
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 
-        //option.deselectByVisibleText();if there is any string inside you can use this one.
-
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 
     }
+
 }
+
